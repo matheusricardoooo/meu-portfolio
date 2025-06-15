7c5d2cf6bf78bd040ba9d2aeadb3e41a6b0e0591
matheusricardooooo <matheusricardodev@gmail.com>
2025-06-15 00:28:15 -0300

    função dos botões dos cards de projetos adicionado

--- script.js
+++ script.js
@@ -12,3 +12,27 @@
     function contact() {
     document.getElementById("contact").scrollIntoView({ behavior: "smooth"})
 }
+
+    function openRepoProject1() {
+        window.open("https://github.com/matheusricardoooo/tela-login", "_blanck")
+}
+
+    function openWindowProject1() {
+        window.open("https://login-gilt-two.vercel.app/", "_blanck")
+}
+
+    function openRepoProject2() {
+        window.open("https://github.com/matheusricardoooo/super-pet", "_blanck")
+}
+
+    function openWindowProject2() {
+        window.open("https://superpet-dun.vercel.app/", "_blanck")
+}
+
+    function openRepoProject3() {
+        window.open("https://github.com/matheusricardoooo/link-bio", "_blanck")
+}
+
+    function openWindowProject3() {
+        window.open("https://link-bio-eosin.vercel.app/", "_blanck")
+}
